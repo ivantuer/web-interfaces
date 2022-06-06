10f26a7065571c5576946b4758a678b7ff60654c
Ivan Hrynevytskyi <35626257+ivantuer@users.noreply.github.com>
2022-06-06 11:20:22 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-perf/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-perf',
+    '@typescript-eslint',
+    'simple-import-sort',
+    'import',
+    'unused-imports',
+    'react-hooks',
+    'prettier',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'react-hooks/exhaustive-deps': 'error',
+  },
+};
+
+// VSCODE Settings
+
+// "[typescriptreact]": {
+//   "editor.defaultFormatter": "esbenp.prettier-vscode",
+//   "editor.codeActionsOnSave": {
+//     "source.fixAll": true,
+//   },
+//   "editor.formatOnSave": true
+// },
